53de4208eccb1f749f3559d37b3ddf9c615805d1
Vikram Oberoi <voberoi@gmail.com>
2013-02-13 17:18:21 -0500

    Gah, need to bump the version to re-upload to PyPi. Or do I?

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = []
 
 setup(
     name='python-magento',
-    version='0.2.1',
+    version='0.2.2',
     author='Vikram Oberoi',
     author_email='voberoi@gmail.com',
     packages=['magento'],
