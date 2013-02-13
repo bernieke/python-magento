a20ecd7d842d81f418a876fa29562349491c4474
Vikram Oberoi <voberoi@gmail.com>
2013-02-13 17:13:53 -0500

    Bunch of changes to setup.py. Version bump.

--- CHANGES.md
+++ CHANGES.md
@@ -1,1 +1,2 @@
-v0.1.0, 2/13/2013 -- Initial release
+v0.1.0, 2/13/2013 -- Initial release.
+v0.2.0, 2/13/2013 -- Docs. Extra methods.
--- setup.py
+++ setup.py
@@ -3,19 +3,33 @@ try:
 except ImportError:
     from distutils.core import setup
 
+packages = ['magento']
+requires = []
+
 setup(
     name='python-magento',
-    version='0.1.0',
+    version='0.2.1',
     author='Vikram Oberoi',
     author_email='voberoi@gmail.com',
     packages=['magento'],
+    install_requires=requires,
     entry_points={
         'console_scripts': [
             'magento-ipython-shell = magento.magento_ipython_shell:main'
         ]
     },
-    url='http://pypi.python.org/pypi/python-magento/',
-    license='LICENSE.md',
+    url='https://github.com/voberoi/python-magento',
+    license=open('LICENSE.md').read(),
     description='A Python wrapper to Magento\'s XML-RPC API.',
     long_description=open('README.rst').read(),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',        
+    )
 )
