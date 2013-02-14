b7a0bcfade38eb9a6dc135c66b2ffdf8ab4fe73e
Vikram Oberoi <voberoi@gmail.com>
2013-02-14 14:34:01 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = []
 
 setup(
     name='python-magento',
-    version='0.2.2',
+    version='0.2.3',
     author='Vikram Oberoi',
     author_email='voberoi@gmail.com',
     packages=['magento'],
